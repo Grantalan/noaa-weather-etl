8eddbd5a37b543f3b1828bb914884ba8da3c6206
Grant Alan <Grantmodels.ai@gmail.com>
2026-07-12 20:31:12 -0500

    adjust load_df(), add statement to save processed pivot table

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from etl.load import get_engine, load_df
 
 engine = get_engine()
 
-
 twentytwenty_dly = extract()
 twentytwenty_dly = transform(twentytwenty_dly)
 load_df(engine, twentytwenty_dly, "daily_2020")
-
 
 stations = extract_stations()
 stations.to_csv('data/processed/stations.csv', index=False)
