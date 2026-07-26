f3ef02a2775c5b608faf75a2108f61d6b4cb36e4
Grant Alan <Grantmodels.ai@gmail.com>
2026-07-26 18:00:08 -0500

    Use TypeAdapter to validate data type

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from etl.extract import extract_current_year, extract_stations
 from etl.transform import transform
-from etl.load import get_engine, load_df
+from etl.load import get_engine, load_df, upsert_daily
 
 if __name__ == '__main__':
     engine = get_engine()
 
     current_dly = extract_current_year()
     current_dly = transform(current_dly)
-    load_df(engine, current_dly, "daily_actual")
+    upsert_daily(engine, current_dly, "daily_actual")
 
     stations = extract_stations()
     stations.to_csv('data/processed/stations.csv', index=False)
